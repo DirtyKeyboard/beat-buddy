7dd7bf2264b3a7c157bb637eea29c6c383892ffc
DirtyKeyboard <andrewxx000222@icloud.com>
2023-05-12 13:44:59 -0500

    ws

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
         target: 'https://beat-buddy-api.onrender.com',
         changeOrigin: true,
         secure: false,
+        ws: true,
         rewrite: path => path.replace(/^\/api/, '')
       }
     }
